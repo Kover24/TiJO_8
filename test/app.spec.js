ffad5d85fd329ae1048dd0f2adc5574d6bf8c693
Kover24 <klich27150@student.pwsztar.edu.pl>
2017-06-07 00:09:34 +0200

    Mutation tests 2nd raport

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,11 +8,17 @@ describe('reverseNumber', function ()
 it ('should reverse number', function(){
   expect(app.reverseNumber(12345)).to.eql(54321);
 });
+  it ('should return false', function(){
+    expect(app.reverseNumber()).to.eql(false);
+  });
 });
 
 describe('returnOnlyLetter', function () {
   it('should return only letters', function () {
     expect(app.returnOnlyLetter('a7b6c2A3B5C123')).to.eql('abc');
+  });
+  it('should return false', function () {
+    expect(app.returnOnlyLetter(123)).to.eql(false);
   });
 });
 
@@ -20,5 +26,8 @@ describe('isEmail', function ()
 {
   it('should return false', function () {
     expect(app.isEmail('bartlomiejklich.gmail')).to.eql(false);
+  });
+  it('should return true', function () {
+    expect(app.isEmail('bartlomiejklich@gmail.com')).to.eql(true);
   });
 });
